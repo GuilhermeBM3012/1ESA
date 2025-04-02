8f06492f2bd00bce5a33c11f8956ef9fdabbc1ce
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-02 09:53:32 -0300

    aula01

---
+++ JavaScript/aula01/scripts.js
@@ -0,0 +1,5 @@
+console.log('Ola meu amigo Node.js');
+let soma = 5 + 4;
+console.log('O resultado da soma é ' + soma);
+// console.log('O resultado da soma é ', soma);
+// console.log(´O resultado sa soma é ${soma}´)
