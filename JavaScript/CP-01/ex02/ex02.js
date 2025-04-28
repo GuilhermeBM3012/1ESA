46812d3ba83f113057883c761dc9aed37462c195
Guilherme Barone Milani <guibarone.milani@gmail.com>
2025-04-28 14:26:58 -0300

    Update ex02.js

--- JavaScript/CP-01/ex02/ex02.js
+++ JavaScript/CP-01/ex02/ex02.js
@@ -1,31 +1,23 @@
-function calcularIMC() {
+function calcular() {
     const peso = parseFloat(document.getElementById('peso').value);
     const altura = parseFloat(document.getElementById('altura').value);
-   
-    if (isNaN(peso) || isNaN(altura) || altura <= 0) {
-      document.getElementById('resultadoIMC').innerText = "Por favor, insira valores válidos.";
-      document.getElementById('categoriaIMC').innerText = "";
-      return;
+
+    const res = peso / (altura * altura)
+    document.getElementById('Resultado').innerHTML = `Seu IMC é ${res.toFixed(2)}`;
+
+    let categoria = '';
+    if (res < 18.5) {
+        categoria = 'Abaixo do peso';
+    } else if (18.5 < res < 24.9) {
+        categoria = 'Peso normal';
+    } else if ( 25 < res < 29.9) {
+        categoria = 'Sobrepeso';
+    } else if (30 < res < 34.9) {
+        categoria = 'Obesidade grau 1';
+    } else {
+        categoria = 'Obesidade grau 2';
     }
-   
-    const imc = peso / (altura * altura);
-    document.getElementById('resultadoIMC').innerText = `Seu IMC é ${imc.toFixed(2)}`;
-   
-    let categoria = "";
-   
-    if (imc < 18.5) {
-        categoria = "Abaixo do peso";
-      } else if (imc < 25.0) {
-        categoria = "Peso normal";
-      } else if (imc < 30.0) {
-        categoria = "Sobrepeso";
-      } else if (imc < 35.0) {
-        categoria = "Obesidade grau 1";
-      } else if (imc < 40.0) {
-        categoria = "Obesidade grau 2";
-      } else {
-        categoria = "Obesidade grau 3";
-      }
-     
-    document.getElementById('categoriaIMC').innerText = `Categoria: ${categoria}`;
-  }
+
+    document.getElementById('categoria').innerHTML = `Sua categoria é ${categoria}`;
+
+}
