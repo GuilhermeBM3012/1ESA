dc44e3946e4b5e1dfe4ac6ea3b5a0ca2193bb7a6
Guilherme Barone Milani <guibarone.milani@gmail.com>
2025-04-28 14:32:14 -0300

    Update ex03.js

--- JavaScript/CP-01/ex03/ex03.js
+++ JavaScript/CP-01/ex03/ex03.js
@@ -1,15 +1,12 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-   input: process.stdin,
-   output: process.stdout,
-});
-
-rl.question('Digite um número: ', (input) => {
-    const numero = parseFloat(input);
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+readline.question('Diga um numero: ', (num) => {
+    const numero = parseInt(num);
 
     if (!isNaN(numero)) {
-        if (numero === 0) {
+        if (numero == 0) {
             console.log('Seu numero é zero');
         } else if (numero > 0) {
             console.log('Seu numero é positivo');
