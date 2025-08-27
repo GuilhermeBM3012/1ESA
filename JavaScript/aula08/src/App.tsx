afc3efdd1091ea6d7f8ad2461fc7476b357d820b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-27 09:42:16 -0300

    aula08

---
+++ JavaScript/aula08/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Tarefa from './pages/tarefa/Tarefa';
+
+function App() {
+
+  return (
+    // <> </> sao fragmentos 
+    <>
+        <Tarefa></Tarefa>
+    </>
+  );
+}
+
+export default App;
