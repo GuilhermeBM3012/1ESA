dd60980fcd0f836d3c443a92291818e9032bfccc
Guilherme Barone Milani <guibarone.milani@gmail.com>
2025-09-03 18:34:20 -0300

    Update ListaExercicios.py

--- Python/aula11/ListaExercicios.py
+++ Python/aula11/ListaExercicios.py
@@ -21,20 +21,69 @@ def criaMatriz(linhas, colunas):
 
 
 # 3 -
+def mostrar_matriz(matriz):
+    for linhas in matriz:
+        print(linhas)
+    return
+
+def criar_matriz(linhas, colunas):
+    matriz = []
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            linha.append(0)
+        matriz.append(linha)
+    return matriz
+
 def diagonal(matriz):
     for i in range(len(diagonal)):
         diagonal[i][i] = 1
     return
 
+matriz = criar_matriz(50, 50)
+diagonal(matriz)
+mostrar_matriz(matriz)
+
 
 # 4 -
+def mostrar_matriz(matriz):
+    for linhas in matriz:
+        print(linhas)
+    return
+
+def criar_matriz(linhas, colunas):
+    matriz = []
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            linha.append(0)
+        matriz.append(linha)
+    return matriz
+
 def contra_diagonal(matriz):
     for i in range(len(matriz)):
         matriz[i][len(matriz) - i - 1] = 1
     return
 
+matriz = criar_matriz(50, 50)
+diagonal(matriz)
+mostrar_matriz(matriz)
 
 # 5 -
+def mostrar_matriz(matriz):
+    for linhas in matriz:
+        print(linhas)
+    return
+
+def criar_matriz(linhas, colunas):
+    matriz = []
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            linha.append(0)
+        matriz.append(linha)
+    return matriz
+
 def triangulo(matriz):
     for i in range(len(matriz)):
         for j in range(i + 1, len(matriz[0])):
@@ -42,8 +91,25 @@ def triangulo(matriz):
             matriz[j][i] = 2
         return
 
+matriz = criar_matriz(50, 50)
+diagonal(matriz)
+mostrar_matriz(matriz)
 
 # 6 -
+def mostrar_matriz(matriz):
+    for linhas in matriz:
+        print(linhas)
+    return
+
+def criar_matriz(linhas, colunas):
+    matriz = []
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            linha.append(0)
+        matriz.append(linha)
+    return matriz
+
 def matriz_transposta(matriz):
     for i in range(len(matriz)):
         for j in range(i):
@@ -52,11 +118,28 @@ def matriz_transposta(matriz):
             matriz[j][i] = aux
     return
 
+matriz = criar_matriz(50, 50)
+diagonal(matriz)
+mostrar_matriz(matriz)
 
 # 7 -
 
 
 # 8 -
+def mostrar_matriz(matriz):
+    for linhas in matriz:
+        print(linhas)
+    return
+
+def criar_matriz(linhas, colunas):
+    matriz = []
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            linha.append(0)
+        matriz.append(linha)
+    return matriz
+
 def xadrez(matriz):
     for i in range(len(xadrez)):
         for j in range(len(xadrez[0])):
@@ -66,8 +149,26 @@ def xadrez(matriz):
                 xadrez[i][j] = 1
     return
 
+matriz = criar_matriz(50, 50)
+diagonal(matriz)
+mostrar_matriz(matriz)
+
 
 # 9 -
+def mostrar_matriz(matriz):
+    for linhas in matriz:
+        print(linhas)
+    return
+
+def criar_matriz(linhas, colunas):
+    matriz = []
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            linha.append(0)
+        matriz.append(linha)
+    return matriz
+
 def circulo(matriz):
     circulo = criaMatriz(1000, 1000)
     raio = len(circulo)/2
@@ -79,7 +180,35 @@ def circulo(matriz):
                 circulo[i][j] = 0
     return
 
+matriz = criar_matriz(50, 50)
+diagonal(matriz)
+mostrar_matriz(matriz)
 
 # 10 -
+def mostrar_matriz(matriz):
+    for linhas in matriz:
+        print(linhas)
+    return
 
+def criar_matriz(linhas, colunas):
+    matriz = []
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            linha.append(0)
+        matriz.append(linha)
+    return matriz
+
+def xadrez(matriz):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[0])):
+            if i % 2 == j % 2:
+                matriz[i][j] = 'vaga'
+            else:
+                matriz[i][j] = 'ocupada'
+    return
+
+matriz = criar_matriz(50, 50)
+xadrez(matriz)
+mostrar_matriz(matriz)
 
